daa58e5c3cf337008ab1194be0a9e35c737c2217
TristanEDU <tristanedu@protonmail.com>
2025-12-06 15:27:28 -0800

    Add infantry unit data structure with terrain effects and combat stats

---
+++ assets/units.js
@@ -0,0 +1,53 @@
+const infantry = {
+	militia: {
+		Levels: {
+			1: {
+        TerrainEffect: {
+          Plains: {
+            HP: 20,
+            Armor: Unarmored,
+            Speed: null,
+            Strength: null
+          },
+
+          hills: {
+            HP: 20,
+            Armor: Unarmored,
+            Speed: null,
+            Strength: null
+          },
+
+          mountians: {
+            HP: 20,
+            Armor: Unarmored,
+            Speed: null,
+            Strength: null
+          },
+        },
+
+        CombatStats: {
+          Attack: {
+            Unarmored: 3.5,
+            lightArmor: 1,
+            aircraft: 50,
+          },
+          
+          Defense: {
+            Unarmored: 6,
+            lightArmor: 3,
+            aircraft: 5
+          },
+        },
+			},
+		},
+
+		2: {
+			Attack: {
+				Unarmored: 4,
+				lightArmor: 2,
+				aircraft: 3,
+			},
+		},
+	},
+};
+n 
